e767e2a69c6a68f16cb81328ba2017bb329d4042
Aditya Narayan <2005adityagoswami@gmail.com>
2026-05-25 13:03:36 +0530

    intial setup completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,22 +4,23 @@ import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 import { connectToSocket } from "./controllers/socketManager.js";
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const server = createServer(app);
 const io = connectToSocket(server);
-app.use(cors);
+app.use(cors());
 
 app.use(express.json({limit : "40kb"}));
-app.use(express.urlencoded({limmit : "40kb",extended : true}));
+app.use(express.urlencoded({limit : "40kb",extended : true}));
 
 
 main().catch(err => console.log(err));
 
 async function main() {
-  const connectiondb = await mongoose.connect('mongodb+srv://2005adityagoswami_db_user:nAIU2tzcANO9D0SI@mindmesh.kjm6gko.mongodb.net/?appName=MindMesh');
+  const connectiondb = await mongoose.connect(process.env.MONGO_URL);
   console.log(`MONGO connection host : ${connectiondb.connection.host}`);
 }
 
